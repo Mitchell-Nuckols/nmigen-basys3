3ba040e8da9abc6bd83ddb67b3b70b607eb9e879
Mitchell Nuckols <mitchnuckols@gmail.com>
2020-01-26 20:52:00 -0800

    Changed test program. Added (re)programming script

--- Basys3_Blinker.py
+++ Basys3_Blinker.py
@@ -3,25 +3,27 @@ from nmigen.build import Platform
 from basys3 import Basys3Platform
 
 class BlinkyWithDomain(Elaboratable):
+
+    def __init__(self, width):
+        self.width = width
+
     def elaborate(self, platform):
         clk = platform.request("CLK100")
-        led    = platform.request("LD", 0)
-        timer  = Signal(24)
-
-        led1 = platform.request("LD", 1)
-        sw1 = platform.request("SW", 1)
+        timer  = Signal(self.width)
 
         m = Module()
         m.domains.sync = ClockDomain()
         m.d.comb += ClockSignal().eq(clk.i)
         m.d.sync += timer.eq(timer + 1)
-        m.d.comb += led.o.eq(timer[-1])
 
-        m.d.comb += led1.o.eq(sw1.i)
+        for i in range(0,16):
+            sw = platform.request("SW", i)
+            with m.If(~sw.i):
+                m.d.comb += platform.request("LD", i).o.eq(timer[(self.width-1) - i])
 
         return m
 
 if __name__ == "__main__":
     platform = Basys3Platform()
-    platform.build(BlinkyWithDomain(), do_program=True)
+    platform.build(BlinkyWithDomain(38), do_program=True)
 
--- basys3.py
+++ basys3.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-from nmigen.build import Platform, Resource, Pins, Clock, Attrs, Connector
+from nmigen.build import Platform, Resource, Pins, PinsN, Clock, Attrs, Connector
 from nmigen.build.run import LocalBuildProducts
 from nmigen.cli import main_parser, main_runner
 from nmigen.vendor.xilinx_7series import Xilinx7SeriesPlatform
@@ -12,11 +12,47 @@ class Basys3Platform(Xilinx7SeriesPlatform):
     device = "xc7a35t"
     package = "cpg236"
 
-    speed="1"
+    speed = "1"
 
     default_clk = "CLK100"
 
-    connectors=[]
+    # J connectors are layed out looking at the connector head-on [6-1],[12-7]
+    # J[A(0), B(1), C(2), XADC(3)]
+    connectors = [
+
+        # JA
+        Connector(
+            "J",
+            0,
+            " - - G2 J2 L2 J1 "
+            " - - G3 H2 K2 H1 "
+        ),
+
+        # JB
+        Connector(
+            "J",
+            1,
+            " - - B16 B15 A16 A14 "
+            " - - C16 C15 A17 A15 "
+        ),
+
+        # JC
+        Connector(
+            "J",
+            2,
+            " - - P18 N17 M18 K17 "
+            " - - R18 P17 M19 L17 "
+        ),
+
+        # JXADC
+        Connector(
+            "J",
+            3,
+            " - - N2 M2 L3 J3 "
+            " - - N1 M1 M3 K3 "
+        ),
+
+    ]
 
     resources = [
 
@@ -59,7 +95,42 @@ class Basys3Platform(Xilinx7SeriesPlatform):
         Resource("SW", 14, Pins("T1", dir="i"), Attrs(IOSTANDARD="LVCMOS33")),
         Resource("SW", 15, Pins("R2", dir="i"), Attrs(IOSTANDARD="LVCMOS33")),
 
+        # Onboard buttons: BTN[L(0),R(1),U(2),D(3),C(4)]
+        Resource("BTN", 0, Pins("W19", dir="i"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("BTN", 1, Pins("T17", dir="i"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("BTN", 2, Pins("T18", dir="i"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("BTN", 3, Pins("U17", dir="i"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("BTN", 4, Pins("U18", dir="i"), Attrs(IOSTANDARD="LVCMOS33")),
 
+        # 7seg anode/cathodes: AN[0-3] C[A(0),B(1),C(2),D(3),E(4),F(5),G(6),P(7)]
+        Resource("AN", 0, PinsN("U2", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("AN", 1, PinsN("U4", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("AN", 2, PinsN("V4", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("AN", 3, PinsN("W4", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("C", 0, PinsN("W7", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("C", 1, PinsN("W6", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("C", 2, PinsN("U8", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("C", 3, PinsN("V8", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("C", 4, PinsN("U5", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("C", 5, PinsN("V5", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("C", 6, PinsN("U7", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("C", 7, PinsN("V7", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+
+        # VGA port
+        Resource("RED", 0, Pins("G19", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("RED", 1, Pins("H19", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("RED", 2, Pins("J19", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("RED", 3, Pins("N19", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("GRN", 0, Pins("J17", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("GRN", 1, Pins("H17", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("GRN", 2, Pins("G17", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("GRN", 3, Pins("D17", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("BLU", 0, Pins("N18", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("BLU", 1, Pins("L18", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("BLU", 2, Pins("K18", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("BLU", 3, Pins("J18", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("HSYNC", 0, Pins("P19", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
+        Resource("VSYNC", 0, Pins("R19", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
 
     ]
 
